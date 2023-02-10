9418ffae2742aac3094da3456d50f83afd8c63a4
ssuhacho <ssuhacho@gmail.com>
2023-02-10 12:18:26 -0600

    sign in/sign up page

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SignInSide from "./components/SignInSide";
+import SignUpSide from "./components/SignUpSide";
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+      <Routes>
+        <Route path="/" exact element={<SignInSide />} />
+        <Route exact path="/signup" element={<SignUpSide/> } />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
