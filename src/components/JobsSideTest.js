cbace414da15a33ea8b466c633a5d803681de9b5
KalebAJL <91762487+KalebAJL@users.noreply.github.com>
2023-03-09 20:59:16 -0600

    Fully Function Admin Job Page

--- src/components/JobsSideTest.js
+++ src/components/JobsSideTest.js
@@ -1,156 +1,257 @@
 import { useEffect, useState } from 'react';
-import {db} from './firebase-config';
-import {collection,getDocs, addDoc, doc, updateDoc, deleteDoc} from "firebase/firestore"
+import { db } from './firebase-config';
+import { collection, getDocs, addDoc, doc, updateDoc, deleteDoc, query, where } from "firebase/firestore"
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import { Table as table } from '@material-ui/core';
+import { Battery1Bar } from '@mui/icons-material';
 
 function JobsSide() {
-    const [jobs, setJobs] = useState([])
-    const [courseID, setCourseID] = useState("");
-    const [term, setTerm] = useState("");
-    const [type, setType] = useState("");
-    const [numPositions, setNumPosition] = useState("");
-    const [state, setState] = useState("");
-    const UsersCollectionRef = collection(db, "backenddata")
-  
-    useEffect(() => {
-      const getUsersData = async () => {
-        const data = await getDocs(UsersCollectionRef)
-        setJobs(data.docs.map((elem) => ({ ...elem.data(), id: elem.id })))
+  // Define state variables to manage form input and job data
+  const [jobs, setJobs] = useState([]);
+  const [courseID, setCourseID] = useState("");
+  const [term, setTerm] = useState("");
+  const [type, setType] = useState("");
+  const [numPositions, setNumPosition] = useState("");
+  const [state, setState] = useState("");
+
+  // Define a reference to the "backenddata" collection in Firestore
+  const UsersCollectionRef = collection(db, "backenddata");
+
+  // Use the useEffect hook to fetch data from Firestore when the component mounts
+  useEffect(() => {
+    getUsersData();
+  }, []);
+
+  // Fetch data from Firestore and update the state variable "jobs"
+  const getUsersData = async () => {
+    const data = await getDocs(UsersCollectionRef);
+    setJobs(data.docs.map((elem) => ({ ...elem.data(), id: elem.id })));
+  };
+
+  // Add a new job to Firestore
+  const createJob = async () => {
+    await addDoc(UsersCollectionRef, {
+      Courseid: courseID,
+      Term: term,
+      Type: type,
+      NumPositions: numPositions,
+      State: state,
+    });
+  };
+
+  const handleUpdate = async () => {
+    // Create an object with the updated data for the document
+    const updatedData = {
+      Courseid: courseID,
+      Term: term,
+      Type: type,
+      NumPositions: numPositions,
+      State: state,
+    };
+    // Call the updateJob function with the courseID and updated data
+    await updateJob(courseID, updatedData);
+  };
+
+  // Define a function that updates a document in Firebase with the specified ID
+  const updateJob = async (id, updatedData) => {
+    try {
+      // Query Firebase for documents that match the specified courseID
+      const jobQuerySnapshot = await getDocs(
+        query(UsersCollectionRef, where("Courseid", "==", id))
+      );
+      // If there is at least one document that matches the query
+      if (jobQuerySnapshot.docs.length > 0) {
+        // Get the reference to the first document that matches the query
+        const jobRef = jobQuerySnapshot.docs[0].ref;
+        // Update the document with the updated data
+        await updateDoc(jobRef, updatedData);
+        console.log("Document successfully updated!");
+      } else {
+        console.log(`No job found with id ${id}`);
       }
-  
-      getUsersData()
-    }, [])
-  
-    const CreateUser = async () => {
-      await addDoc(UsersCollectionRef, { Courseid: courseID, Term : term, Type : type, NumPositions : numPositions, State : state})
+      // Reload the page to show the updated data
+      window.location.reload();
+    } catch (error) {
+      console.error("Error updating document: ", error);
     }
-  
-    const CheckUser = async () => {
-      await addDoc(UsersCollectionRef, { Courseid: courseID, Term : term, Type : type, NumPositions : numPositions, State : state})
-      window.location.reload()
+  };
+  const handleDelete = async () => {
+    await deleteJob(courseID);
+  };
+
+  const deleteJob = async (id) => {//delete a job with the selected courseid
+    try {
+      const jobQuerySnapshot = await getDocs(
+        // Query Firebase for documents that match the specified courseID
+        query(UsersCollectionRef, where("Courseid", "==", id))
+      );
+      if (jobQuerySnapshot.docs.length > 0) {
+        // If there is at least one document that matches the query
+        await deleteDoc(jobQuerySnapshot.docs[0].ref);
+        // delete the document
+        console.log("Document successfully deleted!");
+      } else {
+        console.log(`No job found with id ${id}`);
+      }
+      window.location.reload();
+    } catch (error) {
+      console.error("Error deleting document: ", error);
     }
-  
-    const updateDB = async (id, courseID) => {
-      const userDoc = doc(db, "backenddata", id)
-      const NewDoc = { Courseid: courseID, Term : term,Type : type, NumPositions : numPositions, State : state}
-      console.log("Updated the Data on System")
-      await updateDoc(userDoc,NewDoc)
-      console.log("Updated the Data on the Server")
-      window.location.reload()
-    }
-  
-  
-  
-    return (
-        <Container component="main" maxWidth="xs">
-      <div className="Jobs">
-        <Typography component="h1" variant="h5">
-          Admin Job Page
-        </Typography>
-            <Grid container spacing={2}>
-                
-                <Grid item xs={12} sm={15}>
-                    <TextField
+  };
+
+
+
+
+  // Check user
+  // Define an asynchronous function called "checkUser"
+  const checkUser = async () => {
+    // Use the "addDoc" function to add a new document to the "UsersCollectionRef" collection in Firestore
+    // The new document will contain the values of the "courseID", "term", "type", "numPositions", and "state" variables
+    await addDoc(UsersCollectionRef, {
+      Courseid: courseID,
+      Term: term,
+      Type: type,
+      NumPositions: numPositions,
+      State: state,
+    });
+
+    // Reload the page after the new document has been added
+    window.location.reload();
+  };
+  return (
+    <div className='split-screen'>
+      <div className='split-screen-left' >
+        <container fullWidth>
+          <>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <h2 style={{ textAlign: 'center' }}>Edit Jobs</h2>
+              <Grid container spacing={2} justify="center">
+                <Grid item xs={12} sm={10}>
+                  <TextField
+                    variant='outlined'
+                    size='medium'
+                    fullWidth
                     name='courseID'
                     placeholder='CourseID'
-                    onChange={(event) => { setCourseID(event.target.value)}}
-                    />
-                    </Grid>
-            </Grid>
-                <br/><br/>
-                <Grid container spacing={2}>
-                <Grid item xs={12} sm={15}>
-                <TextField
-                  name='term'
-                  placeholder='Term'
-                  onChange={(event) => { setTerm(event.target.value)}}
-                />
-                
-                <br/><br/>
+                    onChange={(event) => { setCourseID(event.target.value) }}
+                  />
                 </Grid>
-            </Grid>
-                <Grid container spacing={2}>
-                <Grid item xs={12} sm={15}>
-                <TextField
-                  name='type'
-                  placeholder='Type'
-                  onChange={(event) => { setType(event.target.value)}}
-                />
-                <br/><br/>
+              </Grid>
+              <Grid container spacing={2} justify="center">
+                <Grid item xs={12} sm={10}>
+                  <TextField
+                    variant='outlined'
+                    size='medium'
+                    fullWidth
+                    name='term'
+                    placeholder='Term'
+                    onChange={(event) => { setTerm(event.target.value) }}
+                  />
                 </Grid>
-            </Grid>
-                <Grid container spacing={2}>
-                <Grid item xs={12} sm={15}>
-                <TextField
-                  name='numPositions'
-                  placeholder='NumPositions'
-                  onChange={(event) => { setNumPosition(event.target.value)}}
-                />
-                <br/><br/>
+              </Grid>
+
+              <Grid container spacing={2} justify="center">
+                <Grid item xs={12} sm={10}>
+                  <TextField
+                    variant='outlined'
+                    size='medium'
+                    fullWidth
+                    name='type'
+                    placeholder='Type'
+                    onChange={(event) => { setType(event.target.value) }}
+                  />
                 </Grid>
-            </Grid>
-                <Grid container spacing={2}>
-                <Grid item xs={12} sm={15}>
-                <TextField
-                  name='state'
-                  placeholder='State'
-                  onChange={(event) => { setState(event.target.value)}}
-                />
-                <br/><br/>
+              </Grid>
+
+              <Grid container spacing={2} justify="center">
+                <Grid item xs={12} sm={10}>
+                  <TextField
+                    variant='outlined'
+                    size='medium'
+                    fullWidth
+                    name='numPositions'
+                    placeholder='NumPositions'
+                    onChange={(event) => { setNumPosition(event.target.value) }}
+                  />
                 </Grid>
-            </Grid>
-                <button onClick={CreateUser}>Add to DB</button>
-                <button onClick={updateDB}>Update DB</button>
-                <br/><br/>
-                <Typography component="h1" variant="h5">
-          Current Jobs
-          
-        </Typography>
-        <div className='grid grid-cols-2'>
-        <Container component="main" maxWidth="xs">
-                     <Grid container spacing={2}>
-                <Grid item xs={12} sm={15}>
-                    <table>
-                <theader>
-            <th className='w-auto text-center'>Course ID</th>
-            <th className='w-auto text-center'>Term</th>
-            <th className='w-auto text-center'>State</th>
-            <th className='w-auto text-center'>Type</th>
-            <th className='w-auto text-center'>State</th>
-            </theader>
-            </table>
-            </Grid>
-            </Grid>
-            </Container>
-                            <br></br>
-            {jobs.map(user => {
-                return <div className='hover:animate-pulse m-4 bg-gray-600 w-1/4 rounded-md p-2'>
-                    <Container component="main" maxWidth="xs">
-                     <Grid container spacing={2}>
-                <Grid item xs={12} sm={15}>
-                    <table class = "jobTable">
-                        <tbody>
-                            <td className='w-auto text-center'>{user.Courseid}</td>
-                            <td className='w-auto text-center'>{user.Term}</td>
-                            <td className='w-auto text-center'>{user.Type}</td>
-                            <td className='w-auto text-center'>{user.NumPositions}</td>
-                            <td className='w-auto text-center'>{user.State}</td>
-                        </tbody>
-                    </table>
-                    </Grid>
-            </Grid>
-            </Container>
-                </div>
-            })}
-        </div>
-      
-        </div>
+              </Grid>
+
+              <Grid container spacing={3} justify="center">
+                <Grid item xs={12} sm={10}>
+                  <TextField
+                    variant='outlined'
+                    size='medium'
+                    fullWidth
+                    name='state'
+                    placeholder='State'
+                    onChange={(event) => { setState(event.target.value) }}
+                  />
+                </Grid>
+              </Grid>
+            </div>
+            <button onClick={createJob} className="button">
+              Add to DB
+            </button>
+            <button onClick={handleUpdate} className="button">
+              Update User
+            </button>
+            <button onClick={handleDelete} className="button">
+              Delete Job
+            </button>
+            <br /><br />
+          </>
+        </container>
+      </div>
+      <div className='split-screen-right'>
+        <Container fullWidth>
+          <h2 style={{ textAlign: 'center', marginRight: '175px' }}>Current Jobs</h2>
         </Container>
-    );
-        }
-  
-  export default JobsSide;
+
+        {jobs.map(user => {
+          return (
+            <div className='hover:animate-pulse m-4 bg-gray-600 w-1/4 rounded-md p-2'>
+              <Container component="main" maxWidth="xs">
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={15}>
+                    <table className='jobTable'>
+                      <thead>
+                        <tr>
+                          <th>Course ID</th>
+                          <th>Term</th>
+                          <th>Type</th>
+                          <th>Num Positions</th>
+                          <th>State</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>{user.Courseid}</td>
+                          <td>{user.Term}</td>
+                          <td>{user.Type}</td>
+                          <td>{user.NumPositions}</td>
+                          <td>{user.State}</td>
+                          <td>
+                            <button>View</button> {/* todo add function */}
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </Grid>
+                </Grid>
+              </Container>
+              <br></br>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  );
+
+
+
+
+}
+
+export default JobsSide;
