fa65866257a6c72cfe841d48d40d5d00258e5d8f
nsg7gv <70167456+nsg7gv@users.noreply.github.com>
2023-04-26 11:44:00 -0500

    Add files via upload

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,43 +1,25 @@
 import * as React from 'react';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import SearchIcon from '@mui/icons-material/Search';
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import umkcLogo from '../../src/assets/images/umkclogo.png';
-import { useContext } from 'react';
-import UserContext from './UserContext';
-
+import NestedList from './nestedList';
+import JobCard from './card';
 
 const pages = ['GTA Certification', 'Course Descriptions'];
+const pageLinks = ['https://catalog.umkc.edu/course-offerings/', 'https://catalog.umkc.edu/general-graduate-academic-regulations-information/international-graduate-student-academic-regulations/'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const drawerWidth = 240;
@@ -88,28 +70,10 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
-function ResponsiveAppBar(props) {
+function ResponsiveAppBar() {
 
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const { user } = useContext(UserContext);
-  const navigate = useNavigate();
-
-const handleAdminClick = () => {
-  navigate('/admin');
-};
-
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -171,10 +135,26 @@ const handleAdminClick = () => {
                 }}
               >
                 {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <MenuItem key={page} onClick={handleCloseNavMenu} component={Link} to="https://catalog.umkc.edu/course-offerings/">
                     <Typography textAlign="center">{page}</Typography>
                   </MenuItem>
                 ))}
+                <Box sx={{ flexGrow: 0 }}>
+                  <Button
+                    //color='inherit'
+                    style={{ backgroundColor: '#005293', color: 'white' }}
+                    component={Link} to="https://catalog.umkc.edu/general-graduate-academic-regulations-information/international-graduate-student-academic-regulations/"
+                  >GTA Certification
+                  </Button>
+              </Box>
+              <Box sx={{ flexGrow: 0 }}>
+                  <Button
+                    //color='inherit'
+                    style={{ backgroundColor: '#005293', color: 'white' }}
+                    component={Link} to="https://catalog.umkc.edu/course-offerings/"
+                  >Course Descriptions
+                  </Button>
+              </Box>
               </Menu>
             </Box>
             <Typography
@@ -203,54 +183,30 @@ const handleAdminClick = () => {
   />
 </Typography>
 
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-              {pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  {page}
-                </Button>
-              ))}
-            </Box>
+            
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                  <Button
+                    //color='inherit'
+                    style={{ backgroundColor: '#005293', color: 'white' }}
+                    component={Link} to="https://catalog.umkc.edu/general-graduate-academic-regulations-information/international-graduate-student-academic-regulations/"
+                  >GTA Certification
+                  </Button>
+                  <Button
+                    //color='inherit'
+                    style={{ backgroundColor: '#005293', color: 'white' }}
+                    component={Link} to="https://catalog.umkc.edu/course-offerings/"
+                  >Course Descriptions
+                  </Button>
+              </Box>
 
             <Box sx={{ flexGrow: 0 }}>
-            {user && user.IsAdmin && (
-  <Button onClick={handleAdminClick} style={{ backgroundColor: '#005293', color: 'white' }}>
-    Admin
-  </Button>
-)}
-
-
-
-
-
-{!user && (
-  <Button
-    //color='inherit'
-    style={{ backgroundColor: '#005293', color: 'white' }}
-    component={Link}
-    to="/signin"
-  >
-    Log In
-  </Button>
-)}
-
-{user && (
-  <Button
-    //color='inherit'
-    style={{ backgroundColor: '#005293', color: 'white' }}
-    component={Link}
-    to="/signup"
-  >
-    Log out
-  </Button>
-)}
-
-</Box>
-
-
+              <Button
+                //color='inherit'
+                style={{ backgroundColor: '#005293', color: 'white' }}
+                component={Link} to="/signin"
+              >Log In
+              </Button>
+            </Box>
           </Toolbar>
         </Container>
       </AppBar>
@@ -264,77 +220,12 @@ const handleAdminClick = () => {
       >
         <Toolbar />
         <Box sx={{ overflow: 'auto' }}>
-          <List sx={style} component="nav" aria-label="mailbox folders">
-            <ListItem>
-              <Search>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                  placeholder="Search…"
-                  inputProps={{ 'aria-label': 'search' }}
-                />
-              </Search>
-            </ListItem>
-            <Divider />
-            <ListItem button>
-              <ListItemText primary="Degree" />
-            </ListItem>
-            <Divider />
-            <ListItem button>
-              <ListItemText primary="Graduating Semester" />
-            </ListItem>
-            <Divider />
-            <ListItem button divider>
-              <ListItemText primary="Graduating Position" />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary="Class" />
-            </ListItem>
-            <Divider />
-            <ListItem button>
-              <ListItemText primary="GTA Certification" />
-            </ListItem>
-            <Divider />
-          </List>
+          <NestedList/>
         </Box>
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Toolbar />
-        <Card sx={{ maxWidth: 345 }}>
-          <CardHeader
-
-            title="CS 101 Grader"
-          />
-          <CardActions disableSpacing>
-            <IconButton aria-label="add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="share">
-              <ShareIcon />
-            </IconButton>
-            <ExpandMore
-              expand={expanded}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </ExpandMore>
-          </CardActions>
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-              <Typography paragraph>
-                <ul>
-                  <li>Undergraduates: You may only apply to be a grader for a course you have taken at UMKC.</li>
-                  <li>Master’s Students: You may only apply to be a grader for undergraduate courses you took during your, previous, course of study. You may only apply to grade for graduate-level classes you have taken at UMKC (and received a satisfactory grade of an A,  A- or B+).</li>
-                  <li>PhD Students: You may be considered as a grader for any class, based on feedback from your advisor</li>
-                </ul>
-                <Button>Apply</Button>
-              </Typography>
-            </CardContent>
-          </Collapse>
-        </Card>
+        <JobCard/>
       </Box>
     </Box>
   );
