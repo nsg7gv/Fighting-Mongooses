4ce00441e23343e155c00c920ee14d20436d2454
KalebAJL <91762487+KalebAJL@users.noreply.github.com>
2023-04-30 20:13:05 -0500

    Updated Admin Selection

--- src/components/jobForm.js
+++ src/components/jobForm.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { TextField, Grid, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { TextField, Grid, Typography, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
 
 
-const JobForm = ({ setCourseID, setTerm, setType, setNumPosition, setState, setDegree, setGradSemester, setClassType }) => (
-  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-    
-    <Typography variant="h4" align="center" style={{ marginBottom: '2rem' }}>
-      Manage Jobs
-    </Typography>
-    <Grid container spacing={2} justify="center">
+const JobForm = ({ setCourseID, setTerm, setType, setNumPosition, setState, setDegree, setGradSemester, setClassType }) => {
+  const [classTypeSelected, setClassTypeSelected] = useState(false);
+
+  const handleClassTypeChange = (event) => {
+    setClassTypeSelected(event.target.value !== '');
+    setClassType(event.target.value);
+  }
+
+  const [degreeSelected, setDegreeSelected] = useState(false);
+
+  const handleDegreeChange = (event) => {
+    setDegreeSelected(event.target.value !== '');
+    setDegree(event.target.value);
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Typography variant="h4" align="center" style={{ marginBottom: '2rem' }}>
+        Manage Jobs
+      </Typography>
+      <Grid container spacing={2} justify="center">
       <Grid item xs={12} sm={10}>
         <TextField
           variant="outlined"
@@ -72,15 +86,23 @@ const JobForm = ({ setCourseID, setTerm, setType, setNumPosition, setState, setD
                 </Grid>
               </Grid>
               <Grid container spacing={3} justify="center">
-      <Grid item xs={12} sm={10}>
-        <TextField
-          variant='outlined'
-          size='medium'
-          fullWidth
-          name='degree'
-          placeholder='Degree Required'
-          onChange={(event) => { setDegree(event.target.value) }}
-        />
+        <Grid item xs={12} sm={10}>
+          <FormControl fullWidth variant="outlined" size="medium">
+        {degreeSelected ? null : <InputLabel>Degree Required</InputLabel>}
+          <Select
+            variant="outlined"
+            size="medium"
+            fullWidth
+            name="degree"
+            value={setDegree}
+            onChange={handleDegreeChange}
+          >
+            <MenuItem value="">None</MenuItem>
+            <MenuItem value="Bachelor's">Bachelor's</MenuItem>
+            <MenuItem value="Master's">Master's</MenuItem>
+            <MenuItem value="Doctorate">Doctorate</MenuItem>
+          </Select>
+        </FormControl>
       </Grid>
     </Grid>
     <Grid container spacing={3} justify="center">
@@ -96,17 +118,27 @@ const JobForm = ({ setCourseID, setTerm, setType, setNumPosition, setState, setD
       </Grid>
     </Grid>
     <Grid container spacing={3} justify="center">
-      <Grid item xs={12} sm={10}>
-        <TextField
-          variant='outlined'
-          size='medium'
-          fullWidth
-          name='classType'
-          placeholder='Class Type'
-          onChange={(event) => { setClassType(event.target.value) }}
-        />
+        <Grid item xs={12} sm={10}>
+          <FormControl fullWidth variant="outlined" size="medium">
+            {classTypeSelected ? null : <InputLabel>Select Class Type</InputLabel>}
+            <Select
+              variant="outlined"
+              size="medium"
+              fullWidth
+              name="classType"
+              value={setClassType}
+              onChange={handleClassTypeChange}
+            >
+              <MenuItem value="">None</MenuItem>
+              <MenuItem value="Computer Science">Computer Science</MenuItem>
+              <MenuItem value="Information Technology">Information Technology</MenuItem>
+              <MenuItem value="Electrical & Computer Engineering">Electrical & Computer Engineering</MenuItem>
+              <MenuItem value="Electrical Engineering">Electrical Engineering</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
       </Grid>
-    </Grid>
-  </div>
-);
+    </div>
+  );
+}
 export default JobForm;
